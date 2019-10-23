90ea42d68d0cb70dcf31fb5e08cf6130547c6bf8
jackmqiu <jackqiu2016@gmail.com>
2019-10-22 17:10:01 -0700

    Movie Detail blocked out

--- src/js/views/components/MovieDetail/MovieDetail.js
+++ src/js/views/components/MovieDetail/MovieDetail.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+
 import MovieDetail from './MovieDetail.styled';
 import api from 'Utils/api';
+
 
 class MovieDetailComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       movie: null,
+      cast: null,
     };
     console.log(this.props);
   }
@@ -14,29 +21,51 @@ class MovieDetailComponent extends React.Component {
     api.get(`/movie/${this.props.match.params.movie_id}`).then((response) => {
       this.setState({ movie: response.data });
     });
+    api.get(`/movie/${this.props.match.params.movie_id}/credits`).then((response) => {
+      this.setState({ cast: response.data.cast });
+    });
   }
 
   render() {
     const {
-      movie
+      movie,
+      cast,
     } = this.state;
-    console.log(movie);
+    console.log(cast);
     return (
       <div>
-        {this.state.movie ?
+        {this.state.movie && this.state.cast ?
           <MovieDetail.Container>
-            <MovieDetail.Brief>
+            <MovieDetail.CapContainer>
               <MovieDetail.Poster>
                 <img src={`https://image.tmdb.org/t/p/w154${movie.poster_path}`} />
               </MovieDetail.Poster>
-              <MovieDetail.Info>
+              <MovieDetail.InfoContainer>
                 <MovieDetail.Title>
                   { movie.title }
                 </MovieDetail.Title>
-              </MovieDetail.Info>
-            </MovieDetail.Brief>
+                { movie.overview }
+              </MovieDetail.InfoContainer>
+              <MovieDetail.VotesContainer>
+                { movie.vote_average }
+              </MovieDetail.VotesContainer>
+            </MovieDetail.CapContainer>
             <MovieDetail.Cast>
-
+              <Table aria-label="Cast">
+                <TableBody>
+                  { cast.map( actor => (
+                    <TableRow key={actor.id}>
+                      <TableCell component="th" scope="row">
+                        <img src={`https://image.tmdb.org/t/p/w45${actor.profile_path}`} />
+                      </TableCell>
+                      <TableCell align="right"> {actor.name} </TableCell>
+                      <TableCell align="right"> {actor.character} </TableCell>
+                    </TableRow>
+                    ))
+                  }
+                  </TableBody>
+              </Table>
+              
             </MovieDetail.Cast>
 
           </MovieDetail.Container>
--- src/js/views/components/MovieDetail/MovieDetail.styled.js
+++ src/js/views/components/MovieDetail/MovieDetail.styled.js
@@ -5,31 +5,46 @@ const Container = styled.div`
   overflow: hidden;
 `;
 
-const Brief = styled.div`
-  width: 60%
+const CapContainer = styled.div`
+  width: 60%;
+  background: gray;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
 `;
 
 const Poster = styled.div`
-  width: 60%
+  background: cyan;
+  margin: 0 auto;
+  grid-column: 1 / 2
 `;
 
-const Info = styled.div`
-  width: 60%
+const InfoContainer = styled.div`
+  background: yellow;
+  grid-column: 2 / 5;
 `;
 
 const Title = styled.div`
-  width: 60%
+  background: pink;
+`;
+
+const VotesContainer = styled.div`
+  background: red;
+  grid-column: 5 / 6;
 `;
 
 const Cast = styled.div`
-  width: 60%
+  width: 60%;
+  margin: 0 auto;
+  background: blue;
 `;
 
 export default {
   Container,
-  Brief,
+  CapContainer,
   Poster,
-  Info,
+  InfoContainer,
+  VotesContainer,
   Title,
   Cast,
 };
